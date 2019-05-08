09c45cf5a9bb3ed7c8690b5f4684333dd7f62bcc
huqi <huqi1008301@163.com>
2019-05-08 10:52:27 +0800

    美化首页；优化window挂载全局变量

--- src/router/index.js
+++ src/router/index.js
@@ -56,16 +56,16 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   // 添加动态(菜单)路由
   // 已添加或者当前路由为页面路由, 可直接访问
-  if (!window.SITE_CONFIG['dynamicMenuRoutesHasAdded'] || fnCurrentRouteIsPageRoute(to, pageRoutes)) {
+  if (window.SITE_CONFIG['dynamicMenuRoutesHasAdded'] || fnCurrentRouteIsPageRoute(to, pageRoutes)) {
     return next()
   }
   // 获取菜单列表, 添加并全局变量保存
-  sysMenuService.getNav().then(({ data: res }) => {
-    if (res.code !== 0) {
-      Vue.prototype.$message.error(res.msg)
-      return next({ name: 'login' })
-    }
-    window.SITE_CONFIG['menuList'] = res.data
+  sysMenuService.getNav().then(res => {
+    // if (res.code !== 0) {
+    //   Vue.prototype.$message.error(res.msg)
+    //   return next({ name: 'login' })
+    // }
+    window.SITE_CONFIG['menuList'] = res
     fnAddDynamicMenuRoutes(window.SITE_CONFIG['menuList'])
     next({ ...to, replace: true })
   }).catch(() => {
@@ -124,6 +124,9 @@ function fnAddDynamicMenuRoutes (menuList = [], routes = []) {
       route['path'] = route['name'] = URL.replace(/\//g, '-')
       route['component'] = () => import(`@/views/system/${URL}`)
     }
+    console.group('fnAddDynamicMenuRoutes')
+    console.log(route)
+    console.groupEnd()
     routes.push(route)
   }
   if (temp.length >= 1) {
