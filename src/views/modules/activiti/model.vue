5629234a9965c44378df2583eeaa0ca16e178285
huqi <huqi1008301@163.com>
2019-05-14 10:10:36 +0800

    替换el-card为d2-container

--- src/views/modules/activiti/model.vue
+++ src/views/modules/activiti/model.vue
@@ -1,7 +1,6 @@
 <template>
-  <el-card shadow="never" class="aui-card--fill">
-    <div class="mod-activiti__model">
-      <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+  <d2-container>
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
         <el-form-item>
           <el-input v-model="dataForm.name" :placeholder="$t('model.name')" clearable></el-input>
         </el-form-item>
@@ -17,42 +16,41 @@
         <el-form-item>
           <el-button type="danger" @click="deleteHandle()">{{ $t('deleteBatch') }}</el-button>
         </el-form-item>
-      </el-form>
-      <el-table
-        v-loading="dataListLoading"
-        :data="dataList"
-        border
-        @selection-change="dataListSelectionChangeHandle"
-        @sort-change="dataListSortChangeHandle"
-        style="width: 100%;">
-        <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-        <el-table-column prop="name" :label="$t('model.name')" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="key" :label="$t('model.key')" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="version" :label="$t('model.version')" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="createTime" :label="$t('model.createTime')" header-align="center" align="center" width="180"></el-table-column>
-        <el-table-column prop="lastUpdateTime" :label="$t('model.lastUpdateTime')" header-align="center" align="center" width="180"></el-table-column>
-        <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
-          <template slot-scope="scope">
-            <a :href="getModelerURL(scope.row.id)" target="_blank" class="el-button el-button--text el-button--small">{{ $t('model.design') }}</a>
-            <el-button type="text" size="small" @click="deployHandle(scope.row.id)">{{ $t('model.deploy') }}</el-button>
-            <a :href="getExportURL(scope.row.id)" target="_blank" class="el-button el-button--text el-button--small">{{ $t('export') }}</a>
-            <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">{{ $t('delete') }}</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <el-pagination
-        :current-page="page"
-        :page-sizes="[10, 20, 50, 100]"
-        :page-size="limit"
-        :total="total"
-        layout="total, sizes, prev, pager, next, jumper"
-        @size-change="pageSizeChangeHandle"
-        @current-change="pageCurrentChangeHandle">
-      </el-pagination>
-      <!-- 弹窗, 新增 / 修改 -->
-      <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
-    </div>
-  </el-card>
+    </el-form>
+    <el-table
+      v-loading="dataListLoading"
+      :data="dataList"
+      border
+      @selection-change="dataListSelectionChangeHandle"
+      @sort-change="dataListSortChangeHandle"
+      style="width: 100%;">
+      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table-column prop="name" :label="$t('model.name')" header-align="center" align="center"></el-table-column>
+      <el-table-column prop="key" :label="$t('model.key')" header-align="center" align="center"></el-table-column>
+      <el-table-column prop="version" :label="$t('model.version')" header-align="center" align="center"></el-table-column>
+      <el-table-column prop="createTime" :label="$t('model.createTime')" header-align="center" align="center" width="180"></el-table-column>
+      <el-table-column prop="lastUpdateTime" :label="$t('model.lastUpdateTime')" header-align="center" align="center" width="180"></el-table-column>
+      <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
+        <template slot-scope="scope">
+          <a :href="getModelerURL(scope.row.id)" target="_blank" class="el-button el-button--text el-button--small">{{ $t('model.design') }}</a>
+          <el-button type="text" size="small" @click="deployHandle(scope.row.id)">{{ $t('model.deploy') }}</el-button>
+          <a :href="getExportURL(scope.row.id)" target="_blank" class="el-button el-button--text el-button--small">{{ $t('export') }}</a>
+          <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">{{ $t('delete') }}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      :current-page="page"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="limit"
+      :total="total"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="pageSizeChangeHandle"
+      @current-change="pageCurrentChangeHandle">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </d2-container>
 </template>
 
 <script>
@@ -61,7 +59,7 @@ import AddOrUpdate from './model-add-or-update'
 import Cookies from 'js-cookie'
 import qs from 'qs'
 export default {
-  mixins: [mixinViewModule],
+  mixins: [ mixinViewModule ],
   data () {
     return {
       mixinViewModuleOptions: {
--- src/views/modules/message/mail-template.vue
+++ src/views/modules/message/mail-template.vue
@@ -1,59 +1,57 @@
 <template>
-  <el-card shadow="never" class="aui-card--fill">
-    <div class="mod-message__mail-template">
-      <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
-        <el-form-item>
-          <el-input v-model="dataForm.name" :placeholder="$t('mail.name')" clearable></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button @click="getDataList()">{{ $t('query') }}</el-button>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="configHandle()">{{ $t('mail.config') }}</el-button>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="danger" @click="deleteHandle()">{{ $t('deleteBatch') }}</el-button>
-        </el-form-item>
-      </el-form>
-      <el-table
-        v-loading="dataListLoading"
-        :data="dataList"
-        border
-        @selection-change="dataListSelectionChangeHandle"
-        @sort-change="dataListSortChangeHandle"
-        style="width: 100%;">
-        <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
-        <el-table-column prop="name" :label="$t('mail.name')" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="subject" :label="$t('mail.subject')" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="createDate" :label="$t('mail.createDate')" sortable="custom" header-align="center" align="center" width="180"></el-table-column>
-        <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
-          <template slot-scope="scope">
-            <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t('update') }}</el-button>
-            <el-button type="text" size="small" @click="sendHandle(scope.row.id)">{{ $t('mail.send') }}</el-button>
-            <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">{{ $t('delete') }}</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <el-pagination
-        :current-page="page"
-        :page-sizes="[10, 20, 50, 100]"
-        :page-size="limit"
-        :total="total"
-        layout="total, sizes, prev, pager, next, jumper"
-        @size-change="pageSizeChangeHandle"
-        @current-change="pageCurrentChangeHandle">
-      </el-pagination>
-      <!-- 弹窗, 新增 / 修改 -->
-      <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
-      <!-- 弹窗, 邮件配置 -->
-      <config v-if="configVisible" ref="config"></config>
-      <!-- 弹窗, 发送邮件 -->
-      <send v-if="sendVisible" ref="send" @refreshDataList="getDataList"></send>
-    </div>
-  </el-card>
+  <d2-container>
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.name" :placeholder="$t('mail.name')" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">{{ $t('query') }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="configHandle()">{{ $t('mail.config') }}</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="danger" @click="deleteHandle()">{{ $t('deleteBatch') }}</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      v-loading="dataListLoading"
+      :data="dataList"
+      border
+      @selection-change="dataListSelectionChangeHandle"
+      @sort-change="dataListSortChangeHandle"
+      style="width: 100%;">
+      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table-column prop="name" :label="$t('mail.name')" header-align="center" align="center"></el-table-column>
+      <el-table-column prop="subject" :label="$t('mail.subject')" header-align="center" align="center"></el-table-column>
+      <el-table-column prop="createDate" :label="$t('mail.createDate')" sortable="custom" header-align="center" align="center" width="180"></el-table-column>
+      <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t('update') }}</el-button>
+          <el-button type="text" size="small" @click="sendHandle(scope.row.id)">{{ $t('mail.send') }}</el-button>
+          <el-button type="text" size="small" @click="deleteHandle(scope.row.id)">{{ $t('delete') }}</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      :current-page="page"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="limit"
+      :total="total"
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="pageSizeChangeHandle"
+      @current-change="pageCurrentChangeHandle">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+    <!-- 弹窗, 邮件配置 -->
+    <config v-if="configVisible" ref="config"></config>
+    <!-- 弹窗, 发送邮件 -->
+    <send v-if="sendVisible" ref="send" @refreshDataList="getDataList"></send>
+  </d2-container>
 </template>
 
 <script>
@@ -62,7 +60,7 @@ import AddOrUpdate from './mail-template-add-or-update'
 import Config from './mail-template-config'
 import Send from './mail-template-send'
 export default {
-  mixins: [mixinViewModule],
+  mixins: [ mixinViewModule ],
   data () {
     return {
       mixinViewModuleOptions: {
